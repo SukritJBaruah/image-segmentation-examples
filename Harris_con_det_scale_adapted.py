82d09abcc006f74da6ac46eb07e2a571e9202110
SukritJBaruah <68151213+SukritJBaruah@users.noreply.github.com>
2020-10-02 18:45:42 +0530

    3rd

---
+++ Harris_con_det_scale_adapted.py
@@ -0,0 +1,36 @@
+from matplotlib import pyplot as plt
+
+from skimage import data
+from skimage.feature import corner_harris, corner_subpix, corner_peaks
+from skimage.transform import warp, AffineTransform
+from skimage.draw import ellipse
+from skimage.color import rgb2gray
+from skimage import io
+
+#natural image
+image = io.imread("images/sample.jpg")
+image = rgb2gray(image)
+tform = AffineTransform(scale=(1.0, 0.5))
+image = warp(image, tform.inverse)
+
+#synthetic image in paint
+#image = io.imread("images/synimg.jpg")
+#image = rgb2gray(image)
+#tform = AffineTransform(scale=(0.6, 1.0))
+#image = warp(image, tform.inverse)
+
+# Sheared checkerboard
+#tform = AffineTransform(scale=(2.2, 1.3), rotation=1, shear=0.7, translation=(110, 30))
+#image = warp(data.checkerboard()[:90, :90], tform.inverse, output_shape=(200, 310))
+
+
+coords = corner_peaks(corner_harris(image), min_distance=5, threshold_rel=0.02)
+coords_subpix = corner_subpix(image, coords, window_size=13)
+
+fig, ax = plt.subplots()
+ax.imshow(image, cmap=plt.cm.gray)
+ax.plot(coords[:, 1], coords[:, 0], color='green', marker='o',
+        linestyle='None', markersize=6)
+ax.plot(coords_subpix[:, 1], coords_subpix[:, 0], '+r', markersize=15)
+ax.axis((0, 480, 310, 0))
+plt.show()
